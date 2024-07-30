c1fb088de235fc9566288db7d3ef14645391f940
Alvaro Olarte <alvaroolartes@hotmail.com>
2024-07-29 20:00:20 -0500

    GitHub, VSCode, Prettier, Astro config

---
+++ .prettierrc.mjs
@@ -0,0 +1,15 @@
+export default {
+    plugins: ['prettier-plugin-astro'],
+    semi: true,
+    singleQuote: false,
+    tabWidth: 4,
+    useTabs: true,
+    overrides: [
+        {
+            files: '**/*astro',
+            options: {
+                parser: 'astro',
+            },
+        },
+    ],
+};
